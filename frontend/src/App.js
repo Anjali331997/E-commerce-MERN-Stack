ac994c6e8b1d5af9052e93ac08c41f8a32bc7dd2
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-21 13:14:00 +0530

    adding routes-still incomplete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Shop from './pages/Shop';
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product'
 function App() {
   return (
     <div>
-     <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop/>} />
+          <Route path='/mens' element={<ShopCategory category="men"/>} />
+          <Route path='/womens' element={<ShopCategory category="women"/>} />
+          <Route path='/kids' element={<ShopCategory category="kid"/>} />
+          <Route path='/product' element={<Product/>}>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
