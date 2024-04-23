3e0ae97dd4308d8a5232e74dc287a5034bbd7f9f
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-23 17:50:31 +0530

    footer css complete

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import './Footer.css'
+import footer_logo from '../Assests/logo_big.png'
+import instagram_icon from '../Assests/instagram_icon.png'
+import pinterest_icon from '../Assests/pintester_icon.png'
+import whatsapp_icon from '../Assests/whatsapp_icon.png'
 
 const Footer = () => {
   return (
-    <div>
-        
+    <div className='footer'>
+        <div className="footer-logo">
+            <img src={footer_logo} alt="footer_logo" />
+            <p>SHOPPER</p>
+        </div>
+        <ul className="footer-links">
+            <li>Company</li>
+            <li>Offices</li>
+            <li>Products</li>
+            <li>About</li>
+            <li>Conatct</li>
+        </ul>
+        <div className="footer-socials-icon">
+            <div className="footer-icons-container">
+                <img src={instagram_icon} alt="instagram" />
+            </div>
+            <div className="footer-icons-container">
+                <img src={pinterest_icon} alt="pinterest" />
+            </div>
+            <div className="footer-icons-container">
+                <img src={whatsapp_icon} alt="whatsapp" />
+            </div>
+        </div>
+        <div className="footer-copyright">
+            <hr />
+            <p>Copyright @ 2024 - All Right Reserved.</p>
+        </div>
     </div>
   )
 }
