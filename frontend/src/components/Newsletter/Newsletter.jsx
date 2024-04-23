8c9f6d91d132fb7b45672c0e22733ae1096c8467
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-23 13:22:58 +0530

    lost track of the updates. pushing all the code into github for newletters

--- frontend/src/components/Newsletter/Newsletter.jsx
+++ frontend/src/components/Newsletter/Newsletter.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Newsletter.css'
 
 const Newsletter = () => {
   return (
-    <div>
-        
+    <div className='newsletter'>
+       <h1>Get Exclusive Offers On Your Email</h1>
+        <p>Subscribe to our newletter and stay updated</p>
+        <div>
+            <input type="email" placeholder='Your Email id' />
+            <button>Subscribe</button>
+        </div>
     </div>
   )
 }
