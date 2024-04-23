fc2865c2f08d3b4cdaa9d30a64854ae2ae90d45e
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-23 19:10:30 +0530

    shopping category

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,11 +1,11 @@
-import React, { createContext } from "react";
+import React ,{createContext} from "react";
 import all_product from "../components/Assests/all_product";
 
-export const ShopContext = createContext(null);
+export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
-    const contextValue = { all_product };
+    const contextValue = {all_product};
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -5,7 +5,10 @@ import dropdown_icon from '../components/Assests/dropdown_icon.png'
 import Item from '../components/items/Items'
 
 const ShopCategory = (props) => {
-  const { all_products } = useContext(ShopContext)
+
+  const {all_product}  = useContext(ShopContext);
+  console.log(all_product);
+
   return (
     <div className='shop-category'>
       <img src={props.banner} alt="banner" />
@@ -19,7 +22,7 @@ const ShopCategory = (props) => {
         </div>
         <div className="shopcategory-products">
           {
-            all_products.map((item, i) => {
+            all_product.map((item, i) => {
               if (props.category === item.category) {
                 return <Item  key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
               }
