b6965d190e2125ed3970ff4693609d4a9bae8d4b
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-23 12:18:29 +0530

    creating the offers banner css is pending in clusding links

--- frontend/src/components/Offer/Offer.jsx
+++ frontend/src/components/Offer/Offer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Offer.css'
+import exclusive_image from '../Assests/exclusive_image.png'
 
 const Offer = () => {
   return (
-    <div>
-        
+    <div className='offers'>
+
+      <div className="offers-left">
+        <h2>Exclusive</h2>
+        <h2>Offers For You</h2>
+        <p>ONLY ON BEST SELLERS PRODUCTS</p>
+        <button>Check Now</button>
+      </div>
+      <div className="offers-right">
+        <img src={exclusive_image} alt="" />
+      </div>
+
     </div>
   )
 }
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import { Popular } from '../components/Popular/Popular'
+import Offer from '../components/Offer/Offer'
 
 const Shop = () => {
   return (
     <div>
         <Hero/>
         <Popular/>
+        <Offer/>
     </div>
   )
 }
