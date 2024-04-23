4dbe4d29e61ba53468002c14f93056ced78dcca9
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-23 18:41:01 +0530

    shop category section css pending

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ import Product from './pages/Product'
 import Cart from './pages/Cart'
 import LoginSignup from './pages/LoginSignup'
 import Footer from './components/footer/Footer';
+import men_banner from './components/Assests/banner_mens.png';
+import women_banner from './components/Assests/banner_women.png';
+import kid_banner from './components/Assests/banner_kids.png';
+
 
 function App() {
   return (
@@ -15,9 +19,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory category="men" />} />
-          <Route path='/womens' element={<ShopCategory category="women" />} />
-          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
           <Route path='/product' element={<Product />}>
             <Route path=':productid' element={<Product/>} />
           </Route>
--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../components/Assests/dropdown_icon.png'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const {all_products} = useContext(ShopContext)
   return (
-    <div>
-        
+    <div className='shop-category'>
+        <img src={props.banner} alt="banner" />
+        <div className="shopcategory-indexsort">
+
+          <p>
+            <span>showing 1-12</span> out of 36 products
+          </p>
+          <div className="shopcategory-sort">
+            Sort by <img src={dropdown_icon} alt="" />
+          </div>
+        </div>
     </div>
   )
 }
