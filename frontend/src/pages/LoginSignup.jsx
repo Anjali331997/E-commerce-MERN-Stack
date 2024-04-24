09e67a2343a6d3ac5514403ad57ebfeb0643bdec
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-24 11:12:24 +0530

    adding the login and signup code

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 const LoginSignup = () => {
   return (
-    <div>
-        
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Allready have an account?
+          <span>Login</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy</p>
+        </div>
+      </div>
     </div>
   )
 }
