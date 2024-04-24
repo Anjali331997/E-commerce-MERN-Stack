4d28ea934481264a6339de1b69a55d87d34a3edf
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-24 19:15:18 +0530

    adding description box for the product page

---
+++ frontend/src/components/Descriptionbox/DescriptionBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Descriptionbox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'></div>
+  )
+}
+
+export default DescriptionBox
