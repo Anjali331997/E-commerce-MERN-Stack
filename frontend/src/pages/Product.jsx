17475b3b319b7d3cbe3e9f8c049f6fef9e3cfd00
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-24 12:05:15 +0530

    adding breadcrum in projects

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
 
 const Product = () => {
+
+  const { all_product } = useContext(ShopContext)
+  const { productId } = useParams();
+  const product = all_product.find((e) => {
+    return (e.id === Number(productId))
+  })
   return (
     <div>
-        
+
     </div>
   )
 }
