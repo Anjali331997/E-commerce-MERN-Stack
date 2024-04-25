d371e4dd56912c2c29a42336c00180de6632abd2
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-25 18:41:18 +0530

    cartitems adding

--- frontend/src/components/CartItems/CartItems.jsx
+++ frontend/src/components/CartItems/CartItems.jsx
@@ -18,24 +18,54 @@ const CartItems = () => {
             </div>
             <hr />
             {
-                
+
                 all_product.map((e) => {
                     if (cartItem[e.id] > 0) {
-                        return  (<div>
-                            <div className="cartitems-format">
+                        return (<div>
+                            <div className="cartitems-format cartitems-format-main">
                                 <img className='carticon-product-icon' src={e.image} alt="" />
                                 <p>{e.name}</p>
                                 <p>${e.new_price}</p>
                                 <button className='cartitems-quantity'>{cartItem[e.id]}</button>
-                                <p>{e.new_price * cartItem[e.id]}</p>
-                                <img src={remove_icon} onClick={() => {
+                                <p>${e.new_price * cartItem[e.id]}</p>
+                                <img className='cartitems-remove-icon' src={remove_icon} onClick={() => {
                                     removeFromCart(e.id)
                                 }} alt="remove" />
                             </div>
                         </div>)
                     }
+                    return null;
                 })
             }
+            <div className="cartitems-down">
+                <div className="cartitems-total">
+                    <h1>Cart Totals</h1>
+                    <div>
+                        <div className="cartitems-total-item">
+                            <p>Subtotal</p>
+                            <p>${0}</p>
+                        </div>
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <p>Shipping Fee</p>
+                            <p>Free</p>
+                        </div>
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <h3>Total</h3>
+                            <h3>${0}</h3>
+                        </div>
+                    </div>
+                    <button>PROCEED TO CHECKOUT</button>
+                </div>
+                <div className="cartitems-promocode">
+                    <p>If you have a promo code, Enter it here</p>
+                    <div className="cartitem-promobox">
+                        <input type="text" placeholder='CODE' />
+                        <button>Apply code</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
