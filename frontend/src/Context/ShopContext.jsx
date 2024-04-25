0da42c4309aa89eddeba6b33c49a3ac5042c211d
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-25 11:13:17 +0530

    adding the context

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,11 +1,25 @@
-import React ,{createContext} from "react";
+import React, { createContext, useState } from "react";
 import all_product from "../components/Assests/all_product";
 
 export const ShopContext = createContext();
-
+const getDefaultCart = () => {
+    let cart = {}
+    for (let i = 0; i < all_product.length + 1; i++) {
+        cart[i] = 0
+    }
+    return cart;
+}
 const ShopContextProvider = (props) => {
+    const [cartItem, setCartItem] = useState(getDefaultCart())
 
-    const contextValue = {all_product};
+    const addToCart = (itemId) => {
+        setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+    }
+    const removeFromCart = (itemId) => {
+        setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+    
+    const contextValue = { all_product, cartItem, addToCart, removeFromCart };
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
