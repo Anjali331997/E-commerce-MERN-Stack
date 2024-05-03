bbc20111cbe11fa9a3482b4ec800a29253e9e04f
Anjali331997 <anjaliap3march1997@gmail.com>
2024-05-03 10:09:31 +0530

    first commit of the day

--- frontend/src/components/Popular/Popular.jsx
+++ frontend/src/components/Popular/Popular.jsx
@@ -6,15 +6,21 @@ import Items from '../items/Items'
 export const Popular = () => {
   return (
     <div className='popular'>
-        <h1>POPULAR IN WOMEN</h1>
-        <div className='line'></div>
-        <div className="popular-item">
-          {
-            data_product.map((item,i)=>{
-              return <Items key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-            })
-          }
-        </div>
+      <h1>POPULAR IN WOMEN</h1>
+      <div className='line'></div>
+      <div className="popular-item">
+        {
+          data_product.map((item, i) => {
+            return <Items
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price} />
+          })
+        }
+      </div>
 
     </div>
   )
