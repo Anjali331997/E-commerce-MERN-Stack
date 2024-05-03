9b7bc63e48c235654a87bed2d10d0f9354754cc7
Anjali331997 <anjaliap3march1997@gmail.com>
2024-05-03 12:20:52 +0530

    navbar responsiveness working

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,43 +1,50 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 
 import logo from '../Assests/logo.png'
 import cart_icon from '../Assests/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assests/menu-dropdown-icon.png'
 
 const Navbar = () => {
     const [menu, setMenu] = useState("shop")
-    const {getTotalCartItems} = useContext(ShopContext)
+    const { getTotalCartItems } = useContext(ShopContext)
+    const menuRef = useRef();
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible')
+        e.target.classList.toggle('open');
+    }
     return (
         <div className='navbar'>
             <div className="nav-logo">
                 <img src={logo} alt="" />
                 <p>SHOPPER</p>
             </div>
-            <ul className='nav-menu'>
+            <img className='nav-dropdown' src={nav_dropdown} onClick={dropdown_toggle} alt="" />
+            <ul ref={menuRef} className='nav-menu'>
                 <li onClick={() => {
                     setMenu("shop")
                 }}>
-                    <Link to='/' style={{textDecoration:'none',color:'var(--grey)'}}>Shop</Link>
+                    <Link to='/' style={{ textDecoration: 'none', color: 'var(--grey)' }}>Shop</Link>
                     {menu === "shop" ? <hr /> : <></>}
                 </li>
                 <li onClick={() => {
                     setMenu("mens")
                 }}>
-                    <Link to='/mens' style={{textDecoration:'none',color:'var(--grey)'}}>Men</Link>
+                    <Link to='/mens' style={{ textDecoration: 'none', color: 'var(--grey)' }}>Men</Link>
                     {menu === "mens" ? <hr /> : <></>}
                 </li>
                 <li onClick={() => {
                     setMenu("womens")
                 }}>
-                    <Link to='/womens' style={{textDecoration:'none',color:'var(--grey)'}}>Women</Link>
+                    <Link to='/womens' style={{ textDecoration: 'none', color: 'var(--grey)' }}>Women</Link>
                     {menu === "womens" ? <hr /> : <></>}
                 </li>
                 <li onClick={() => {
                     setMenu("kids")
                 }}>
-                    <Link to='/kids' style={{textDecoration:'none',color:'var(--grey)'}}> Kids</Link>
+                    <Link to='/kids' style={{ textDecoration: 'none', color: 'var(--grey)' }}> Kids</Link>
                     {menu === "kids" ? <hr /> : <></>}
                 </li>
             </ul>
