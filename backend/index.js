344bab4bf675f233f071b056a816e66f87ef6fb2
Anjali331997 <anjaliap3march1997@gmail.com>
2024-05-08 19:28:58 +0530

    using multer

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,29 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-app.get('/',(req,res)=>{
-    res.status(200).send({message:"Testing the backend:E-commerce"})
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "Testing the backend:E-commerce" })
 })
 
 //Image Storage Engine
 
-const storage = multer.diskStorage()
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
 
-app.listen(port,()=>{
-    console.log("Listening to port:",port)
+const upload = multer({ storage: storage })
+//Creating upload Endpoint for images
+app.use('./images', express.static('upload/images'))
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+app.listen(port, () => {
+    console.log("Listening to port:", port)
 })
