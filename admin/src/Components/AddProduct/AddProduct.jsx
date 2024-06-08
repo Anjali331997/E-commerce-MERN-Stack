2e902c1f6f0cd797e6492c3c5e4d4c9b4ed12ebd
Anjali331997 <anjaliap3march1997@gmail.com>
2024-06-08 18:28:17 +0530

    upload image logic is done. logic to add the image url that is fetched from backend api is succesfully created

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -1,27 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddProduct.css'
 import upload_area from '../../assets/upload_area.svg'
 
 const AddProduct = () => {
+    const [image, setImage] = useState(false)
+    const [newProduct, setnewProduct] = useState({
+        name: "",
+        image: "",
+        category: "women",
+        old_price: 0,
+        new_price: 0
+    })
+
+    const changeHandler = (e) => {
+        setnewProduct({ ...newProduct, [e.target.name]: e.target.value })
+    }
+
+    const imagehandler = (e) => {
+        setImage(e.target.files[0]);
+    }
+
+
+    const Add_Product = async () => {
+        console.log(newProduct);
+        let responseData;
+        let product = newProduct;
+
+        let formData = new FormData();
+        formData.append('product', image);
+        console.log(formData)
+
+        try {
+
+            const response = await fetch('http://localhost:4000/upload',{
+                method:'POST',
+                headers:{
+                    Accept:'appliaction/json'
+                },
+                body:formData,
+            });
+
+            const data =await response.json();
+            responseData=data;
+            if (responseData.success) {
+                product.image = responseData.image_url;
+                console.log(product);
+            }
+
+        } catch (err) {
+            console.log(err);
+        }
+
+        
+    }
+
     return (
         <div className='add-product'>
             <div className="addproduct-itemfield">
                 <p>Product Name</p>
-                <input type="text" name='name' placeholder='Enter product name' />
+                <input value={newProduct.name} onChange={changeHandler} type="text" name='name' placeholder='Enter product name' />
             </div>
             <div className='addproduct-price'>
                 <div className="addproduct-itemfield">
-                    <p>Product price</p>
-                    <input type="text" name='old_price' placeholder='Enter old price' />
+                    <p>Product actual price</p>
+                    <input value={newProduct.old_price} type="text" name='old_price' placeholder='Enter old price' onChange={changeHandler} />
                 </div>
                 <div className="addproduct-itemfield">
                     <p>Product offer price</p>
-                    <input type="text" name='new_price' placeholder='Enter new price' />
+                    <input value={newProduct.new_price} type="text" name='new_price' placeholder='Enter new price' onChange={changeHandler} />
                 </div>
             </div>
             <div className="addproduct-itemfield">
                 <p>Product category</p>
-                <select name="categopry" id="" className='add-product-selector'>
+                <select value={newProduct.category} name="category" id="" className='add-product-selector' onChange={changeHandler} >
                     <option value="women">Women</option>
                     <option value="men">Men</option>
                     <option value="kid">Kid</option>
@@ -29,12 +80,12 @@ const AddProduct = () => {
             </div>
             <div className="addproduct-itemfield">
                 <label htmlFor="file-input">
-                    <img src={upload_area} className='addproduct-thumbnail-image' alt="" />
+                    <img src={image ? URL.createObjectURL(image) : upload_area} className='addproduct-thumbnail-image' alt="" />
                 </label>
-                <input type="file" name='image' id='file-input' hidden />
-                <button className='addproduct-button'>Add product</button>
+                <input onChange={imagehandler} type="file" name='image' id='file-input' hidden />
 
             </div>
+            <button className='addproduct-button' onClick={Add_Product}>Add product</button>
 
         </div>
     )
