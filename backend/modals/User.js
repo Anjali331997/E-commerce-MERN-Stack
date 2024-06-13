1d5c586e3a9c4b4de9eb9a5c8931231446755caa
Anjali331997 <anjaliap3march1997@gmail.com>
2024-06-13 10:14:54 +0530

    user schema is created successsfully

---
+++ backend/modals/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String },
+    email: { type: String, unique: true },
+    password: { type: String },
+    cart: { type: Object },
+    date: { type: Date, default: Date.now }
+})
+
+const User = mongoose.model('user',userSchema)
+
+export default User;
