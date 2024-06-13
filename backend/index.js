bd83623ab4d2d190815abbc1a9860a09e72ba96f
Anjali331997 <anjaliap3march1997@gmail.com>
2024-06-13 10:34:53 +0530

    creating separate routes for users and products

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const multer = require('multer')
 const cors = require('cors')
 const path = require('path')
+const User = require('./modals/User')
 const Product = require('./modals/Product')
 require('dotenv').config();
 require('./config/db');
@@ -34,14 +35,14 @@ app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
         image_url: `${process.env.BACKEND_URL}/images/${req.file.filename}`,
-        statusCheck:"checking"
+        statusCheck: "checking"
     })
 })
 
 
 //adding products
 app.post('/addproduct', async (req, res) => {
-     //logic to create the id by itself.
+    //logic to create the id by itself.
     let products = await Product.find({});
     let new_id;
 
@@ -52,8 +53,8 @@ app.post('/addproduct', async (req, res) => {
         //adding one to the id of last product and assign it into new_id
         new_id = last_product.id + 1;
     }
-    else{
-        new_id=1;
+    else {
+        new_id = 1;
     }
 
     const new_product = new Product({
@@ -63,7 +64,7 @@ app.post('/addproduct', async (req, res) => {
         category: req.body.category,
         new_price: req.body.new_price,
         old_price: req.body.old_price,
-        description:req.body.description
+        description: req.body.description
     });
     await new_product.save();
     console.log("Product Saved")
@@ -74,22 +75,49 @@ app.post('/addproduct', async (req, res) => {
 })
 
 //deleting products
-app.post('/deleteproduct',async (req,res)=>{
+app.post('/deleteproduct', async (req, res) => {
     await Product.findOneAndDelete({
-        id:req.body.id
+        id: req.body.id
     })
     console.log("Item Removed");
     res.status(200).json({
-        success:true,
-        name:req.body.name
+        success: true,
+        name: req.body.name
     })
 })
 
 //creating api to all product from our database
-app.get('/allproducts',async(req,res)=>{
+app.get('/allproducts', async (req, res) => {
     let products = await Product.find({});
     console.log("all products fetched")
     res.status(200).json(products)
+})
+
+//registration
+app.post('/signup', async (req, res) => {
+    const { email, name, password } = req.body;
+    let user = await User.findOne({ email})
+
+    if(user){
+        return res.status(400).json({
+            success:false,
+            error:"existing user found"
+        })
+    }
+
+    let cart = {};
+    for (let i = 0; i < 300;i++){
+        cart[i] = 0;
+    }
+
+    const new_user = new User({
+        name,email,password,cart
+    })
+
+    await new_user.save();
+    
+    
+
 })
 
 
