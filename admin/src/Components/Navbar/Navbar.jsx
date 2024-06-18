f8169f4316e2d5b5d0ab04a0c13163bd2d5b6123
Anjali331997 <anjaliap3march1997@gmail.com>
2024-06-18 20:03:41 +0530

    adding icon to header

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
+import navlogo from '../../assets/Fashionista-logo.png'
 import navprofile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <img src={navlogo} alt="logo" className="nov-logo" />
+            <img src={navlogo} alt="logo" className="nav-logo" />
             <img src={navprofile} alt="Profile-icon" className="nov-profile" />
         </div>
     )
